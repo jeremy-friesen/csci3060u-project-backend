f0227677c6ced23985143d9f13ffd7d53935e297
jeremydavidfriesen <43259835+jeremydavidfriesen@users.noreply.github.com>
2020-03-12 20:38:56 -0400

    initial commit

---
+++ src/lib/Main.java
@@ -0,0 +1,14 @@
+package lib;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO add main loop to iterate DTF and execute transactions
+		
+	}
+	
+	public void parseLine(String line){
+		// TODO
+	}
+
+}
